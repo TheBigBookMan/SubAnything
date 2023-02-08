36b6c37d42bda20c4541c223b27bc6a6095ce841
Ben Smerd <bsmerd@adam.com.au>
2023-02-09 09:09:21 +1030

    update init commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# An app that webscrapes the download csv button a house data for adelaide and then that data is put into a pdf format document and then send it to myself and mum maybe and be like a weekly pdf update of housing prices in adelaide, could be cool. Can have the user input their suburb or the suburb they are curious to receive emails about. Actually have this app as an app that sends out all info to the email, so add in stock info and crypto and business news etc, and the user can tell me what they want and  then just adjust the email address with the properties of what type of emails they want. https://developer.domain.com.au/docs/latest/apis/pkg_properties_locations/references/suburbperformance_get_bynamedsuburb_withoutpostcode
+
+# ? App that webscrapes date/uses API data 
+# ? can be stock prices, business news, housing info/markets
+# ? Will have peoples emails and then have a select criteria of what they want to be sent to them
+# ? WIll need to create a new gmail to send the automatic emails ebcause it will be a lot
+# ? Creates a pdf with the info
+# ? Send weekly pdfs with the info
