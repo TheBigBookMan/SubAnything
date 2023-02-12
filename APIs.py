98dee795a7214944f2550e5c5c7a9c408580a755
Ben Smerd <bsmerd@adam.com.au>
2023-02-12 12:04:30 +1030

    added function so logic seperates between global and country defined news search

--- APIs.py
+++ APIs.py
@@ -1,6 +1,10 @@
 import requests
+from datetime import datetime, timedelta
 
 API_KEY = 'a931d65d2e564a9d9959d549fc4f3d9e'
+today = datetime.today()
+one_week_ago = today - timedelta(days=7)
+date = one_week_ago.strftime("%Y-%m-%d")
 
 def check_api_calls(content):
     # print(content)
@@ -21,29 +25,37 @@ def house_api(queries):
     # ? Houses prices- https://developer.domain.com.au/docs/latest/apis/pkg_properties_locations/references/suburbperformance_get_bynamedsuburb_withoutpostcode
 
 
+
+
 def news_api(queries):
+    def query_by_country(country, query):
+        response = requests.get(f"https://newsapi.org/v2/top-headlines?country={country}&q={query}&apiKey={API_KEY}&language=en")
+        content = response.json()
+        # TODO sort the content
+        # TODO add the content to message
+        print(content)
+
+        
     # TODO make the timeframe from datetime--- from=2023-01-08   this is format of it--- make these automatically one week apart with the to being current day and the from being current day minus 7-- dont need to put n the to beause it will auto be going up to that
     for query in queries:
         query_split = [query.lower() for query in query.split()]
         print(query_split)
         if query_split[0] == 'australian':
             country = 'au'
+            query_by_country(country, query_split[1])
         elif query_split[0] == 'us':
             country = 'us'
+            query_by_country(country, query_split[1])
         elif query_split[0] == 'global':
-            response = requests.get(f"https://newsapi.org/v2/everything?q={query_split[1]}&from=2023-02-05&sortBy=publishedAt&apiKey={API_KEY}&language=en&pageSize=20")
+            response = requests.get(f"https://newsapi.org/v2/everything?q=election&from={date}&sortBy=publishedAt&apiKey={API_KEY}&language=en&pageSize=20")
             content = response.json()
             # TODO sort the content
             # TODO add the content to the message 
             print(content)
             print("global")
-            return
 
-        response = requests.get(f"https://newsapi.org/v2/top-headlines?country={country}&q={query_split[1]}&apiKey={API_KEY}&language=en")
-        content = response.json()
-        # TODO sort the content
-        # TODO add the content to message
-        print(content)
+        
+    
 
 
 def stock_api(queries):
