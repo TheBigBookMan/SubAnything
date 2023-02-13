8caa8bf04cabf153515ffa3d280ed9eeb3de22b8
Ben Smerd <bsmerd@adam.com.au>
2023-02-14 09:44:23 +1030

    added in sorting function for the creating pdf with each data section

--- pdf_creator.py
+++ pdf_creator.py
@@ -3,9 +3,35 @@ from fpdf import FPDF
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False, margin=0)
 
-def create_pdf(data):
-    print("PDFF")
+
+def create_news_section(data):
+    print("NEWS SECTION")
     print(data)
 
-    news_category = data['news']
-    print(news_category)
+
+def create_house_section(data):
+    pass
+
+def create_stock_section(data):
+    pass
+
+def create_crypto_section(data):
+    pass
+
+def create_pdf(data):
+    print("PDFF")
+    # print(data)
+
+    for key, value in data.items():
+        if key == 'house':
+            create_house_section(value)
+        elif key == 'news':
+            create_news_section(value)
+        elif key == 'stocks':
+            create_stock_section(value)
+        elif key == 'crypto':
+            create_crypto_section(value)
+
+
+
+    pdf.output('temp.pdf')
