dfacb6ec7e83e08c8b108cc35efcbe57eb035c51
Ben Smerd <bsmerd@adam.com.au>
2023-02-14 09:54:36 +1030

    added in for loop for the categories and subsections of the categories for each news article to then be printed out on the pdf

--- pdf_creator.py
+++ pdf_creator.py
@@ -7,6 +7,16 @@ pdf.set_auto_page_break(auto=False, margin=0)
 def create_news_section(data):
     print("NEWS SECTION")
     print(data)
+    pdf.set_font(family="Times", style="B", size=20)
+    pdf.cell(w=30, h=10, txt="News: ", align="L", ln=1)
+
+    for category in data:
+        for key, value in category.items():
+            pdf.set_font(family="Times", style="B", size=12)
+            pdf.cell(w=30, h=10, txt=key.title(), align="L", ln=1)
+
+            for news in value:
+                pass
 
 
 def create_house_section(data):
@@ -21,6 +31,7 @@ def create_crypto_section(data):
 def create_pdf(data):
     print("PDFF")
     # print(data)
+    pdf.add_page()
 
     for key, value in data.items():
         if key == 'house':
